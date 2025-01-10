30a5ce1b79fdc8fcb63b30a2aee6335e81ff3f3d
chrisbruchnerutcj <Bruchner.Is.Chris@student.utcluj.ro>
2025-01-10 13:02:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const slides = ["image1.jpg", "image2.jpg", "image3.jpg"];
 let currentSlide = 0;
 
+// Function to update the slide image
+function updateSlide() {
+    document.getElementById('slide').src = slides[currentSlide];
+}
+
 document.getElementById('prev').addEventListener('click', () => {
     currentSlide = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
-    document.getElementById('slide').src = slides[currentSlide];
+    updateSlide();
 });
 
 document.getElementById('next').addEventListener('click', () => {
     currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
-    document.getElementById('slide').src = slides[currentSlide];
+    updateSlide();
+});
+
+document.getElementById('slide').addEventListener('dblclick', () => {
+    currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
+    updateSlide();
 });
